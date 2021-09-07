96f10515376ce32215e582bbd9c3e0287100a0ba
Anastasia <nastiakarachova@gmail.com>
2021-09-07 15:01:18 +0300

    1 lesson finished

---
+++ 01-cards/app.js
@@ -0,0 +1,14 @@
+const slides = document.querySelectorAll('.slide')
+
+const clearActiveClasses = () => {
+  slides.forEach((slide) => {
+    slide.classList.remove('active')
+  })
+}
+
+slides.forEach((slide) => {
+  slide.addEventListener('click', () => {
+    clearActiveClasses()
+    slide.classList.add('active')
+  })
+})
